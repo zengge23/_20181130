edf971607766a51331fc6fe23760c0344bab56cd
zengge23 <zengge23@outlook.com>
2018-12-12 18:11:52 +0800

    无

--- gmall-item-web/src/main/java/com/java/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/java/gmall/item/controller/ItemController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ItemController {
 
@@ -17,6 +20,15 @@ public class ItemController {
     public String test(ModelMap map){
         //取值
         map.put("hello","hello thymaleaf");
+        //判断
+        map.put("num","1");
+        //循环
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<5; i++){
+            list.add("数据"+1);
+        }
+        map.put("list",list);
+        map.put("name","毒液");
 
         return "test";
     }
