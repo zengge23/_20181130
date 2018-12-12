a047b37eada8f7b7ad66ea0e894f3d5e1a3fc0d1
zengge23 <zengge23@outlook.com>
2018-12-12 15:53:36 +0800

    无

--- gmall-item-web/src/main/java/com/java/gmall/item/controller/ItemController.java
+++ gmall-item-web/src/main/java/com/java/gmall/item/controller/ItemController.java
@@ -1,6 +1,7 @@
 package com.java.gmall.item.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +11,13 @@ public class ItemController {
     @RequestMapping("{skuId}.html")
     public String item(@PathVariable String skuId){
         return "item";
+    }
+
+    @RequestMapping("test")
+    public String test(ModelMap map){
+        //取值
+        map.put("hello","hello thymaleaf");
+
+        return "test";
     }
 }
