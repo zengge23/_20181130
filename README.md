8a4c045b436f112f0ab9e86b48515a87a9008f5f
zengge23 <zengge23@outlook.com>
2018-12-14 19:15:15 +0800

    无

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-
+***
 gamll-user-server.port=8070
-gamll-user-web.port=8080
 
+gamll-user-web.port=8080
+***
 gmall-manage-server.port=8071
+
 gmall-manage-web.port=8081
+***
+gmall-item-web:8082
+***
+gmall-list-service:8073
+
+gamll-list-web:8083
+
--- gmall-list-service/src/test/java/com/java/gmall/list/GmallListServiceApplicationTests.java
+++ gmall-list-service/src/test/java/com/java/gmall/list/GmallListServiceApplicationTests.java
@@ -1,16 +1,52 @@
 package com.java.gmall.list;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GmallListServiceApplicationTests {
 
+    @Autowired
+    JestClient jestClient;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException {
+        Search build = new Search.Builder(
+                "{\n" +
+                "  \"query\": {\n" +
+                "   \"bool\": {\n" +
+                "     \"must\": [\n" +
+                "       {\"match\": {\n" +
+                "         \"name\": \"行动\"\n" +
+                "       }}\n" +
+                "     ],\n" +
+                "     \"filter\": {\n" +
+                "       \"term\": {\n" +
+                "         \"actorList.id\": \"3\"\n" +
+                "       }\n" +
+                "     }\n" +
+                "   }\n" +
+                "  }\n" +
+                "}").addIndex("movie_chn").addType("movie").build();
+
+        SearchResult execute = jestClient.execute(build);
+
+        List<SearchResult.Hit<Object,Void>> hits = execute.getHits(Object.class);
+        for(SearchResult.Hit<Object,Void> hit : hits){
+            Object source = hit.source;
+            System.out.println(source);
+        }
+
     }
 
 }
