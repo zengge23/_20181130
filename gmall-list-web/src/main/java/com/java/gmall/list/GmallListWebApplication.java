d93013d899672504cd6d00f56feaa04214653c10
zengge23 <zengge23@outlook.com>
2018-12-15 08:35:55 +0800

    无

---
+++ gmall-list-web/src/main/java/com/java/gmall/list/GmallListWebApplication.java
@@ -0,0 +1,12 @@
+package com.java.gmall.list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallListWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallListWebApplication.class, args);
+    }
+}
---
+++ gmall-user-web/src/main/java/com/java/gmall/user/GmallUserWebApplication.java
@@ -0,0 +1,12 @@
+package com.java.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllUserWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllUserWebApplication.class, args);
+    }
+}
