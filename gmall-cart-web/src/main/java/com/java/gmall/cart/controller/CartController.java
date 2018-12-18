a5e8536b878faca1dd78a9649443286d8be179b0
zengge23 <zengge23@outlook.com>
2018-12-18 18:22:23 +0800

    无

---
+++ gmall-cart-web/src/main/java/com/java/gmall/cart/controller/CartController.java
@@ -0,0 +1,20 @@
+package com.java.gmall.cart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CartController {
+
+    @RequestMapping("addToCart")
+    public String addToCart(String skuId, int num){
+
+        return "redirect:/cartAddSuccess";
+    }
+
+    @RequestMapping("cartAddSuccess")
+    public String cartAddSuccess(){
+
+        return "success";
+    }
+}
